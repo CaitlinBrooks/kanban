da07c0ffe95857622537de5f06919f6fd85b4998
CaitlinCodes <caiit@me.com>
2018-09-05 09:41:49 -0600

    Notes for lists. Edits and changes to component.

--- client/src/components/List.vue
+++ client/src/components/List.vue
@@ -1,12 +1,14 @@
 <template>
-  
+
 </template>
 
 <script>
-export default {
-  name: 'List'
-}
+  export default {
+    name: 'List'
+  }
 </script>
 
 <style scoped>
 </style>
+
+<!-- on the board vue, the getLists method is fired off. Parents fire off the request for their children. Utilize mounted to retrieve the lists -->
