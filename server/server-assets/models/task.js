05c1092a0a8408039ca1cdf74958e2e8368c9a89
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-09-06 16:24:46 -0600

    commented where I think error is in task.js

--- server/server-assets/models/task.js
+++ server/server-assets/models/task.js
@@ -6,6 +6,7 @@ let schemaName = 'Task'
 let schema = new Schema({
   description: { type: String, required: true },
   created: { type: Number, required: true, default: Date.now() },
+  //error here trying to cast to ObjectID
   authorId: { type: ObjectId, ref: 'User', required: true },
   listId: { type: ObjectId, ref: 'List', required: true }
 })
