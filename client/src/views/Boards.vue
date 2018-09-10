cb12c784f54ea668106fd4067c4a9e78a96b9069
CaitlinCodes <caiit@me.com>
2018-09-10 09:23:29 -0600

    board placeholder

--- client/src/views/Boards.vue
+++ client/src/views/Boards.vue
@@ -2,7 +2,7 @@
   <div class="boards">
     WELCOME TO THE BOARDS!!!
     <form @submit.prevent="addBoard">
-      <input type="text" placeholder="title" v-model="newBoard.title" required>
+      <input type="text" placeholder="New Board" v-model="newBoard.title" required>
       <input type="text" placeholder="description" v-model="newBoard.description">
       <button type="submit">Create Board</button>
     </form>
@@ -14,38 +14,38 @@
 </template>
 
 <script>
-export default {
-  name: "boards",
-  created() {
-    //blocks users not logged in
-    if (!this.$store.state.user._id) {
-      this.$router.push({ name: "login" });
-    }
-  },
-  mounted() {
-    this.$store.dispatch("getBoards");
-  },
-  data() {
-    return {
-      newBoard: {
-        title: "",
-        description: ""
+  export default {
+    name: "boards",
+    created() {
+      //blocks users not logged in
+      if (!this.$store.state.user._id) {
+        this.$router.push({ name: "login" });
       }
-    };
-  },
-  computed: {
-    boards() {
-      return this.$store.state.boards;
-    }
-  },
-  methods: {
-    addBoard() {
-      this.$store.dispatch("addBoard", this.newBoard);
-      this.newBoard = { title: "", description: "" };
     },
-    deleteBoard(boardId) {
-      this.$store.dispatch("deleteBoard", boardId);
+    mounted() {
+      this.$store.dispatch("getBoards");
+    },
+    data() {
+      return {
+        newBoard: {
+          title: "",
+          description: ""
+        }
+      };
+    },
+    computed: {
+      boards() {
+        return this.$store.state.boards;
+      }
+    },
+    methods: {
+      addBoard() {
+        this.$store.dispatch("addBoard", this.newBoard);
+        this.newBoard = { title: "", description: "" };
+      },
+      deleteBoard(boardId) {
+        this.$store.dispatch("deleteBoard", boardId);
+      }
     }
-  }
-};
+  };
 </script>
