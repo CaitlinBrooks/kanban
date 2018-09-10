758ac924c54e2695d8bb103c06a95709b86f8727
CaitlinCodes <caiit@me.com>
2018-09-10 10:08:15 -0600

    removed commented area in board vue

--- client/src/views/Board.vue
+++ client/src/views/Board.vue
@@ -5,12 +5,6 @@
       <input type="text" name="title" v-model="listData.title" placeholder="New List">
       <input type="text" name="description" v-model="listData.description" placeholder="description">
       <button class="btn btn-success" type="submit">Send It</button>
-      <!-- 
-      <div v-on:submit.prevent="createComment">
-        <input type="text" name="title" v-model="commentData.title" placeholder="New Comment">
-        <input type="text" name="description" v-model="commentData.description" placeholder="description">
-        <button type="submit">Send It</button>
-      </div> -->
     </form>
     <list :listData="list" v-for="list in lists" :key="list._id" />
     <!-- <router-link :to="{name: 'list', params: {listId: list._id}}">{{list.title}}</router-link>
