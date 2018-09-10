a565093874c425f16fde1b9d9a6d10ab15325320
CaitlinCodes <caiit@me.com>
2018-09-10 09:16:28 -0600

    board title

--- client/src/views/Board.vue
+++ client/src/views/Board.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="board">
-    {{boardId}}
+    {{boardId.title}}
     <form v-on:submit.prevent="createList">
       <input type="text" name="title" v-model="listData.title" placeholder="title">
       <input type="text" name="description" v-model="listData.description" placeholder="description">
